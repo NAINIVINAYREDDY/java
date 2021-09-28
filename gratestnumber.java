474328a259058c51b0e841ac8552dccc9f26bb89
NAINIVINAYREDDY <91367483+NAINIVINAYREDDY@users.noreply.github.com>
2021-09-28 13:31:22 +0530

    Add files via upload

---
+++ gratestnumber.java
@@ -0,0 +1,17 @@
+public class gratestnumber{
+	public static void main(String[] args){
+		int a= 6;
+		int b=24;
+		int c=7;
+		
+		if(a>b && a>c){
+			System.out.print(a+" is the greatest number");
+		}
+		else if(b>a && b>c){
+			System.out.print(b+" is the greatest number");
+		}
+		else{
+			System.out.print(c+" is the greatest number");
+		}
+	}
+}
---
+++ oddeven.java
@@ -0,0 +1,16 @@
+public class oddeven{
+        public static void main(String[] args) {
+            int n = 27;
+            if (n % 2 == 0) {
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+        }
+    }
+
+    
+
+    
+    
+
